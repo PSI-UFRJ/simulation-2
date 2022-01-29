23e180047fe3c2cd206da2f31a5e4088fe91420b
Eduardo Barbosa <eduardo_dsb@dcc.ufrj.com>
2022-01-28 22:41:24 -0300

    feat: adding populationControl script

--- Sims2/Assets/Scripts/PopulationControl.cs
+++ Sims2/Assets/Scripts/PopulationControl.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PopulationControl : MonoBehaviour
 {
@@ -10,11 +12,21 @@ public class PopulationControl : MonoBehaviour
 
     private Environment env;
 
-    [SerializeField] private int numberOfIndividuals = 1;
+    private Transform[] individuals;
+
+    [SerializeField] private int numberOfIndividuals = 0;
 
     [SerializeField] private float weightT = 1;
     [SerializeField] private float weightL = 1;
     [SerializeField] private float weightU = 1;
+    [SerializeField] private float weightP = 1;
+    [SerializeField] private float weightA = 1;
+
+    [SerializeField] private List<float> temperatureUpperLimits;
+    [SerializeField] private List<float> lightUpperLimits;
+    [SerializeField] private List<float> umidityUpperLimits;
+    [SerializeField] private List<int>   plantsUpperLimits;
+    [SerializeField] private List<int>   animalsUpperLimits;
 
     private const int INCREASE = 1;
     private const int DECREASE = -1;
@@ -25,44 +37,173 @@ public class PopulationControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        env = new Environment();
+        individuals = this.gameObject.GetComponentsInChildren<Transform>(true).Where(x => x.parent == this.transform).ToArray();
         SetEnvironment(1, (float)45.5, (float)0.65);
+        IntervenePopulation(env.GetTemperature(), env.GetLight(), env.GetUmidity(), 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        IntervenePopulation();
+        
     }
 
-    private void IntervenePopulation()
+    public void IntervenePopulation(float temperature, float light, float umidity, int numOfPlants, int numOfAnimals)
     {
         // a cada período de tempo aumentar, diminuir ou fazer nada com o número de indivíduos da população
+
+        ControlPopulation(temperature, light, umidity, numOfPlants, numOfAnimals);
+
         if(numberOfIndividuals > 0)
         {
             numberOfIndividuals += INTERVATION;
+
+            if (INTERVATION == INCREASE)
+            {
+                SpawnIndividual();
+            }
+            else if (INTERVATION == DECREASE)
+            {
+                KillIndividual();
+            }
         }
     }
 
-    public void ControlPopulation(float temperature, float light, float umidity)
+    private void SpawnIndividual()
     {
-        float result = (weightT * IntervationTemperature(temperature) + weightL * IntervationLight(light) + weightU * IntervationUmidity(umidity)) / (weightT + weightL + weightU);
+        if ((individuals == null) || (individuals.Length == 0))
+        {
+            return;
+        }
 
-        // Fazer if-else para caso o resultado caia num range, muda o INTERVATION para INCREASE ou DECREASE ou MAINTAIN
+        foreach(Transform t in individuals)
+        {
+            if (!t.gameObject.activeSelf)
+            {
+                t.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    private void KillIndividual()
+    {
+        if ((individuals == null) || (individuals.Length == 0))
+        {
+            return;
+        }
+
+        foreach (Transform t in individuals)
+        {
+            if (t.gameObject.activeSelf)
+            {
+                t.gameObject.SetActive(false);
+                break;
+            }
+        }
+    }
+
+    public void ControlPopulation(float temperature, float light, float umidity, int numOfPlants, int numOfAnimals)
+    {
+        float result = 0;
+
+        if (type == PopulationType.Plant)
+        {
+            result = (weightT * IntervationTemperature(temperature) + weightL * IntervationLight(light) + weightU * IntervationUmidity(umidity)) / (weightT + weightL + weightU);
+        }
+        else if(type == PopulationType.Animal)
+        {
+            result = (weightP * IntervationPlants(numOfPlants) + (weightA * IntervationAnimals(numOfAnimals))) / (weightP + weightA);
+        }
+
+        INTERVATION = (result > 0) ? INCREASE : (result == 0) ? MAINTAIN : DECREASE;
+
     }
 
     private int IntervationTemperature(float temperature) //  Calcular se no ponto de vista da temperatura, a população deveria crescer, diminuir ou ficar igual
     {
-        return INCREASE;
+        if(temperature <= temperatureUpperLimits[0])
+        {
+            return INCREASE;
+        }
+        else if (temperature <= temperatureUpperLimits[1])
+        {
+            return MAINTAIN;
+        }
+        else if (temperature <= temperatureUpperLimits[2])
+        {
+            return DECREASE;
+        }
+        else
+        {
+            return DECREASE;
+        }
     }
 
     private int IntervationLight(float light) //  Calcular se no ponto de vista da luz, a população deveria crescer, diminuir ou ficar igual
     {
-        return DECREASE;
+        if (light <= lightUpperLimits[0])
+        {
+            return INCREASE;
+        }
+        else if (light <= lightUpperLimits[1])
+        {
+            return MAINTAIN;
+        }
+        else if (light <= lightUpperLimits[2])
+        {
+            return DECREASE;
+        }
+        else
+        {
+            return DECREASE;
+        }
     }
 
     private int IntervationUmidity(float umidity) //  Calcular se no ponto de vista da umidade, a população deveria crescer, diminuir ou ficar igual
     {
-        return MAINTAIN;
+        if (umidity <= umidityUpperLimits[0])
+        {
+            return INCREASE;
+        }
+        else if (umidity <= umidityUpperLimits[1])
+        {
+            return MAINTAIN;
+        }
+        else if (umidity <= umidityUpperLimits[2])
+        {
+            return DECREASE;
+        }
+        else
+        {
+            return DECREASE;
+        }
+    }
+
+    private int IntervationPlants(int numOfPlants)
+    {
+        if (numOfPlants <= plantsUpperLimits[0])
+        {
+            return INCREASE;
+        }
+        else if (numOfPlants <= plantsUpperLimits[1])
+        {
+            return MAINTAIN;
+        }
+        else if (numOfPlants <= plantsUpperLimits[2])
+        {
+            return DECREASE;
+        }
+        else
+        {
+            return DECREASE;
+        }
+    }
+    
+    private int IntervationAnimals(int numOfAnimals)
+    {
+        return DECREASE;
     }
 
     public void SetEnvironment(float temperature, float light, float umidity)
@@ -71,6 +212,7 @@ public class PopulationControl : MonoBehaviour
         env.SetTemperature(temperature);
         env.SetUmidity(umidity);
     }
+
 
 }
 
