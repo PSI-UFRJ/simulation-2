75dc589992a89c83f1933d945244cf73229b21e8
Tainá Lima <tainaslima19@gmail.com>
2022-02-09 21:48:13 -0300

    feat: worm animation, sound effects, exit popup

--- Sims2/Assets/Scripts/IntroductionManager.cs
+++ Sims2/Assets/Scripts/IntroductionManager.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
+using System;
 
 public class IntroductionManager : MonoBehaviour
 {
     private bool isPopupOn;
+
+    [SerializeField]
+    private GameObject popupExit;
+
+    [SerializeField]
+    private UnityEngine.UI.Button muteBtn;
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
     // Start is called before the first frame update
     void Start()
     {
+        InitializeIntroduction();
         isPopupOn = false;
     }
 
@@ -17,11 +32,48 @@ public class IntroductionManager : MonoBehaviour
         
     }
 
+    public void InitializeIntroduction()
+    {
+        ActivateModeBtn();
+
+        this.gameObject.GetComponent<SoundManager>().StartSound();
+        this.gameObject.GetComponent<SoundManager>().PlaySound("Environment");
+
+        IniteMuteBtnSprite();
+    }
+
+    public void IniteMuteBtnSprite()
+    {
+        if (AudioListener.pause)
+        {
+            muteBtn.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load("UI Components/Header_MuteBtn_Off", typeof(Sprite)) as Sprite;
+        }
+        else
+        {
+            muteBtn.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load("UI Components/Header_MuteBtn_On", typeof(Sprite)) as Sprite;
+        }
+    }
+
+    public void ActivateModeBtn()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        //UnityEngine.UI.Button introBtn = GameObject.Find("IntroductionModeBtn").GetComponent<UnityEngine.UI.Button>();
+        UnityEngine.UI.Button gameBtn = GameObject.Find("GameModeBtn").GetComponent<UnityEngine.UI.Button>();
+        UnityEngine.UI.Button labBtn = GameObject.Find("LabModeBtn").GetComponent<UnityEngine.UI.Button>();
+
+    }
+
     public void SetPopup(GameObject popup, bool isActive, string message)
     {
         isPopupOn = isActive;
-        Transform popupText = popup.GetComponent<Transform>().Find("Image").Find("Text");
-        popupText.GetComponent<UnityEngine.UI.Text>().text = message;
+        if(message != "")
+        {
+            Transform popupText = popup.GetComponent<Transform>().Find("Image").Find("Text");
+            popupText.GetComponent<UnityEngine.UI.Text>().text = message;
+        }
+
         popup.SetActive(isActive);
     }
 
@@ -30,4 +82,44 @@ public class IntroductionManager : MonoBehaviour
         isPopupOn = false;
         popup.SetActive(false);
     }
+
+    public void ChangeScene(string sceneName)
+    {
+        if (sceneName == "Game")
+        {
+            SceneManager.LoadScene("Game");
+        }
+        else if (sceneName == "Introduction")
+        {
+            SceneManager.LoadScene("Introduction");
+        }
+        else if (sceneName == "Menu")
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public void StartClickSound()
+    {
+        this.gameObject.GetComponent<SoundManager>().PlaySound("Click");
+    }
+
+    public void OnMinimizeButtonClick()
+    {
+        ShowWindow(GetActiveWindow(), 2);
+    }
+
+    #region Close App
+    public void QuitApplication(bool popup = false)
+    {
+        if (popup)
+        {
+            SetPopup(popupExit, true, "");
+            return;
+        }
+
+        Debug.Log("Closed application");
+        Application.Quit();
+    }
+    #endregion
 }
